ad3966195904ad0f319d533138a7c67aeb05bb5c
brandon <lim.wx.brandon@gmail.com>
2023-07-11 00:28:06 +0800

    Populated html elements for calculator

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function add(num1, num2)
+{
+    res = num1 + num2;
+}
+
+function operate(num1, num2, operator)
+{
+    if (operator === "+") add(num1, num2);
+}
+
