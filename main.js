5088b5733f5c858cec3e95c4470d2853804ac2a2
brandon <lim.wx.brandon@gmail.com>
2023-07-13 00:53:21 +0800

    Completed calculator with functionalities

--- main.js
+++ main.js
@@ -1,14 +1,144 @@
 let num1 = 0;
 let num2 = 0;
-let operator = "";
-
-function add(num1, num2)
-{
-    res = num1 + num2;
-}
+let operator_key = "";
+let last_operator = "";
+let operator_pressed = false;
 
 function operate(num1, num2, operator)
 {
-    if (operator === "+") add(num1, num2);
+    let res = 0;
+    if (operator === "add") res = num1 + num2;
+    if (operator === "subtract") res = num1 - num2;
+    if (operator === "divide") res = num1 / num2;
+    if (operator === "multiply") res = num1 * num2;
+
+    return res;
 }
 
+function addNumeric(e, display) 
+{
+    const number = e.target.innerText;
+    const displayPrimary = display.querySelector(".primary-text");
+    if (!displayPrimary.textContent) {
+        if (number === ".") return;
+        displayPrimary.textContent = number;
+    }
+    else {
+        if (number === "." && displayPrimary.textContent.includes(".")) return;
+        displayPrimary.textContent += number;
+    }
+}
+
+function addOperator(e, display)
+{
+    const operator = e.target.innerText;
+    const displayPrimary = display.querySelector(".primary-text");
+    const displaySecondary = display.querySelector(".secondary-text");
+
+    // Add number
+    if (!operator_pressed) {
+        if (!displaySecondary.textContent) {
+            if (displayPrimary.textContent.includes(".")) num1 = parseFloat(displayPrimary.textContent);
+            else num1 = parseInt(displayPrimary.textContent);
+            operator_key = e.target.dataset.action;
+            operator_pressed = true;
+        }
+        else {
+            displaySecondary.textContent += ` ${operator}`;
+            operator_key = e.target.dataset.action;
+            operator_pressed = true;
+        }
+    }
+    else {
+        if (displayPrimary.textContent.includes(".")) num2 = parseFloat(displayPrimary.textContent);
+        else num2 = parseInt(displayPrimary.textContent);
+        last_operator = operator_key;
+        operator_key = e.target.dataset.action;
+        let res = operate(num1, num2, last_operator);
+
+        // Refresh display
+        displaySecondary.textContent = `${res} ${operator}`;
+
+        // Reallocate numbers
+        num1 = res;
+    }
+
+    if (!displaySecondary.textContent) {
+        displaySecondary.textContent = `${displayPrimary.textContent} ${operator}`;
+    }
+    displayPrimary.textContent = "";
+}
+
+function clearDisplay(e, display)
+{
+    const displayPrimary = display.querySelector(".primary-text");
+    const displaySecondary = display.querySelector(".secondary-text");
+    if (e.target.dataset.action == "clear") {
+        num1 = 0;
+        num2 = 0;
+        operator_key = "";
+        last_operator = "";
+        operator_pressed = false;
+        displayPrimary.textContent = "";
+        displaySecondary.textContent = "";
+    }
+    else if (e.target.dataset.action == "delete") {
+        if (displayPrimary.textContent) {
+            displayPrimary.textContent = "";
+        }
+        else {
+            if (operator_pressed) {
+                displaySecondary.textContent = `${num1}`;
+                operator_key = "";
+                operator_pressed = false;
+            }
+        }
+    }
+}
+
+function getResult(display)
+{
+    const displayPrimary = display.querySelector(".primary-text");
+    const displaySecondary = display.querySelector(".secondary-text");
+    
+    if (!operator_pressed) return;
+    if (displayPrimary.textContent.includes(".")) num2 = parseFloat(displayPrimary.textContent);
+    else num2 = parseInt(displayPrimary.textContent);
+    let res = operate(num1, num2, operator_key);
+
+    // Update display
+    displaySecondary.textContent = res;
+    displayPrimary.textContent = "";
+
+    // Reallocate number
+    num1 = res;
+    operator_key = "";
+    last_operator = "";
+    operator_pressed = false;
+}
+
+const keys = document.querySelector(".sub-body");
+const display = document.querySelector(".display");
+
+keys.addEventListener('click', e => {
+    if (e.target.matches("button")) {
+        if (e.target.dataset.action) {
+            if (e.target.dataset.action == "add" || e.target.dataset.action == "subtract" ||
+                e.target.dataset.action == "divide" || e.target.dataset.action == "multiply") {
+                    addOperator(e, display);
+            }
+            else if (e.target.dataset.action == "decimal") {
+                addNumeric(e, display);
+            }
+            else if (e.target.dataset.action == "clear" || e.target.dataset.action == "delete") {
+                clearDisplay(e, display);
+            }
+            else if (e.target.dataset.action == "calculate") {
+                getResult(display);
+            }
+        }
+        else {
+            addNumeric(e, display);
+        }
+    }
+});
